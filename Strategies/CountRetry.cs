4fbab5b5925deef370c31c901f0957419c71dd5b
Yuriy Kushnir <yurko7@hotmail.com>
2017-09-25 00:15:43 +0300

    Add CountRetry strategy that counts number of retries.

---
+++ Strategies/CountRetry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YuKu.Retryable.Strategies
+{
+    public sealed class CountRetry : IRetryStrategy
+    {
+        public CountRetry(Int32 maxTries)
+        {
+            MaxTries = maxTries;
+        }
+
+        public Int32 MaxTries { get; }
+
+        public Int32 Tries { get; private set; }
+
+        public Task<Boolean> PrepareToRetry(Exception exception, CancellationToken cancellationToken)
+        {
+            // Keep counting each try even if their number exceeds MaxTries.
+            Boolean canRetry = Tries++ < MaxTries;
+            return Task.FromResult(canRetry);
+        }
+    }
+}
