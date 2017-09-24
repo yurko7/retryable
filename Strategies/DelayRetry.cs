1af65080cba1a378b33d4707ff6ee75c13c5c5fc
Yuriy Kushnir <yurko7@hotmail.com>
2017-09-25 00:18:00 +0300

    Add DelayRetry strategy that waits specified amount of time before each retry.

---
+++ Strategies/DelayRetry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YuKu.Retryable.Strategies
+{
+    public sealed class DelayRetry : IRetryStrategy
+    {
+        public DelayRetry(TimeSpan delay)
+        {
+            Delay = delay;
+        }
+
+        public TimeSpan Delay { get; }
+
+        public async Task<Boolean> PrepareToRetry(Exception exception, CancellationToken cancellationToken)
+        {
+            await Task.Delay(Delay, cancellationToken);
+            return true;
+        }
+    }
+}
