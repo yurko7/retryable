c9fa01f935028118476481de6b19004c485d57e6
Yuriy Kushnir <yurko7@hotmail.com>
2017-09-25 00:14:21 +0300

    Add ExceptionStrategy that checks exception to determine if retry is allowed.

---
+++ Strategies/ExceptionRetry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YuKu.Retryable.Strategies
+{
+    public sealed class ExceptionRetry : IRetryStrategy
+    {
+        public ExceptionRetry(Func<Exception, Boolean> predicate)
+        {
+            _predicate = predicate;
+        }
+
+        public Task<Boolean> PrepareToRetry(Exception exception, CancellationToken cancellationToken)
+        {
+            Boolean canRetry = _predicate(exception);
+            return Task.FromResult(canRetry);
+        }
+
+        private readonly Func<Exception, Boolean> _predicate;
+    }
+}
