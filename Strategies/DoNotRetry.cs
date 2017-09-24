5253a6e2164b95e17539d6d91eb200e7f736a9ec
Yuriy Kushnir <yurko7@hotmail.com>
2017-09-25 00:12:07 +0300

    Add DoNotRetry strategy that disables retries.

---
+++ Strategies/DoNotRetry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YuKu.Retryable.Strategies
+{
+    public sealed class DoNotRetry : IRetryStrategy
+    {
+        public Task<Boolean> PrepareToRetry(Exception exception, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(false);
+        }
+    }
+}
