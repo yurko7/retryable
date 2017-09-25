78eb6532f9907da1000f3fe343f76f85bac355cd
Yuriy Kushnir <yurko7@hotmail.com>
2017-09-25 21:45:20 +0300

    Add GroupRetry strategy that allows to combine multiple retry strategies.

---
+++ Strategies/GroupRetry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YuKu.Retryable.Strategies
+{
+    public sealed class GroupRetry : IRetryStrategy
+    {
+        public GroupRetry(params IRetryStrategy[] retryStrategies)
+        {
+            _retryStrategies = new List<IRetryStrategy>(retryStrategies);
+        }
+
+        public async Task<Boolean> PrepareToRetry(Exception exception, CancellationToken cancellationToken)
+        {
+            using (List<IRetryStrategy>.Enumerator strategiesEnumerator = _retryStrategies.GetEnumerator())
+            {
+                Boolean canRetry = true;
+                while (canRetry && strategiesEnumerator.MoveNext())
+                {
+                    IRetryStrategy strategy = strategiesEnumerator.Current;
+                    canRetry = await strategy.PrepareToRetry(exception, cancellationToken);
+                }
+                return canRetry;
+            }
+        }
+
+        private readonly List<IRetryStrategy> _retryStrategies;
+    }
+}
