83b5c7bea9a672a1ee3aa5bdd222c46211aaab91
Yuriy Kushnir <yurko7@hotmail.com>
2017-10-01 17:03:31 +0300

    Add binary exponential BackoffRetry strategy with 20% random deviation.

---
+++ Strategies/BackoffRetry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YuKu.Retryable.Strategies
+{
+    public sealed class BackoffRetry : IRetryStrategy
+    {
+        public Int32 Tries { get; private set; }
+
+        public async Task<Boolean> PrepareToRetry(Exception exception, CancellationToken cancellationToken)
+        {
+            Int32 exponent = Tries++ % 31;
+            Int32 delay = (1 << exponent) * 1000;
+            Int32 maxDeviation = delay / 5;
+            Int32 deviation = _random.Next(delay - maxDeviation, delay + maxDeviation);
+            await Task.Delay(delay + deviation, cancellationToken);
+            return true;
+        }
+
+        private readonly Random _random = new Random();
+    }
+}
